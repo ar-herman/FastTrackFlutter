a069d9e8efcd90b6665f67ffb8ff1d9ca2659430
ardiyansahherman <ardiyansahherman92@gmail.com>
2022-09-25 20:42:27 +0800

    status ok

---
+++ visible_invisible/lib/page/home_page.dart
@@ -0,0 +1,324 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Learn"),
+      ),
+      // body: Column(
+      //   children: [
+      //     Container(
+      //       height: 100,
+      //       width: 100,
+      //       color: Colors.orange,
+      //       child: const Center(
+      //         child: Text(
+      //           "Halo",
+      //         ),
+      //       ),
+      //     ),
+      //     Container(
+      //       height: 100,
+      //       width: 100,
+      //       color: Colors.green,
+      //       child: const Center(
+      //         child: Text(
+      //           "Halo",
+      //         ),
+      //       ),
+      //     ),
+      //     Container(
+      //       height: 100,
+      //       width: 100,
+      //       color: Colors.red,
+      //       child: const Center(
+      //         child: Text(
+      //           "Halo",
+      //         ),
+      //       ),
+      //     ),
+      //   ],
+      // ),
+
+      //row
+      // body: Row(
+      //   children: [
+      //     Container(
+      //       height: 100,
+      //       width: 100,
+      //       color: Colors.orange,
+      //       child: const Center(
+      //         child: Text(
+      //           "Halo",
+      //         ),
+      //       ),
+      //     ),
+      //     Container(
+      //       height: 100,
+      //       width: 100,
+      //       color: Colors.green,
+      //       child: const Center(
+      //         child: Text(
+      //           "Halo",
+      //         ),
+      //       ),
+      //     ),
+      //     Container(
+      //       height: 100,
+      //       width: 100,
+      //       color: Colors.red,
+      //       child: const Center(
+      //         child: Text(
+      //           "Halo",
+      //         ),
+      //       ),
+      //     ),
+      //   ],
+      // ),
+
+      //stack
+      // body: Stack(
+      //   children: [
+      //     Container(
+      //       height: 200,
+      //       width: 200,
+      //       color: Colors.orange,
+      //       child: const Center(
+      //         child: Text(
+      //           "Halo",
+      //         ),
+      //       ),
+      //     ),
+      //     Container(
+      //       height: 150,
+      //       width: 150,
+      //       color: Colors.green,
+      //       child: const Center(
+      //         child: Text(
+      //           "Halo",
+      //         ),
+      //       ),
+      //     ),
+      //     Container(
+      //       height: 100,
+      //       width: 100,
+      //       color: Colors.red,
+      //       child: const Center(
+      //         child: Text(
+      //           "Halo",
+      //         ),
+      //       ),
+      //     ),
+      //   ],
+      // ),
+
+      //singlechildscrollview
+      // body: SingleChildScrollView(
+      //   child: Column(
+      //     children: [
+      //       Container(
+      //         height: 100,
+      //         width: 100,
+      //         color: Colors.orange,
+      //         child: const Center(
+      //           child: Text(
+      //             "Halo",
+      //           ),
+      //         ),
+      //       ),
+      //       Container(
+      //         height: 100,
+      //         width: 100,
+      //         color: Colors.green,
+      //         child: const Center(
+      //           child: Text(
+      //             "Halo",
+      //           ),
+      //         ),
+      //       ),
+      //       Container(
+      //         height: 100,
+      //         width: 100,
+      //         color: Colors.red,
+      //         child: const Center(
+      //           child: Text(
+      //             "Halo",
+      //           ),
+      //         ),
+      //       ),
+      //       Container(
+      //         height: 100,
+      //         width: 100,
+      //         color: Colors.yellow,
+      //         child: const Center(
+      //           child: Text(
+      //             "Halo",
+      //           ),
+      //         ),
+      //       ),
+      //       Container(
+      //         height: 100,
+      //         width: 100,
+      //         color: Colors.black,
+      //         child: const Center(
+      //           child: Text(
+      //             "Halo",
+      //           ),
+      //         ),
+      //       ),
+      //       Container(
+      //         height: 100,
+      //         width: 100,
+      //         color: Colors.blue,
+      //         child: const Center(
+      //           child: Text(
+      //             "Halo",
+      //           ),
+      //         ),
+      //       ),
+      //       Container(
+      //         height: 100,
+      //         width: 100,
+      //         color: Colors.amber,
+      //         child: const Center(
+      //           child: Text(
+      //             "Halo",
+      //           ),
+      //         ),
+      //       ),
+      //       Container(
+      //         height: 100,
+      //         width: 100,
+      //         color: Colors.brown,
+      //         child: const Center(
+      //           child: Text(
+      //             "Halo",
+      //           ),
+      //         ),
+      //       ),
+      //       Container(
+      //         height: 100,
+      //         width: 100,
+      //         color: Colors.amberAccent,
+      //         child: const Center(
+      //           child: Text(
+      //             "Halo",
+      //           ),
+      //         ),
+      //       ),
+      //       Container(
+      //         height: 100,
+      //         width: 100,
+      //         color: Colors.orangeAccent,
+      //         child: const Center(
+      //           child: Text(
+      //             "Halo",
+      //           ),
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      // ),
+
+      //listview
+      body: ListView(
+        children: [
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.orange,
+            child: const Center(
+              child: Text(
+                "Halo",
+              ),
+            ),
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.green,
+            child: const Center(
+              child: Text(
+                "Halo",
+              ),
+            ),
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.red,
+            child: const Center(
+              child: Text(
+                "Halo",
+              ),
+            ),
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.orange,
+            child: const Center(
+              child: Text(
+                "Halo",
+              ),
+            ),
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.green,
+            child: const Center(
+              child: Text(
+                "Halo",
+              ),
+            ),
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.red,
+            child: const Center(
+              child: Text(
+                "Halo",
+              ),
+            ),
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.orange,
+            child: const Center(
+              child: Text(
+                "Halo",
+              ),
+            ),
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.green,
+            child: const Center(
+              child: Text(
+                "Halo",
+              ),
+            ),
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.red,
+            child: const Center(
+              child: Text(
+                "Halo",
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
